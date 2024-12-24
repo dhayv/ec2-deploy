9d49308a78fa6c1652add7eb1c56436f9f7e9e4b
Hayvid <davidhyppolitedh@gmail.com>
2024-12-23 20:00:08 -0500

    updated and tested script

---
+++ scripts/before_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Update system
+sudo apt update 
+
+# Enable and install nginx from amazon-linux-extras
+sudo apt-get install nginx -y
+
+# Start and enable nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+# Install Node.js from Amazon
+sudo apt-get install nodejs -y
+
+# Install CodeDeploy Agent
+sudo apt install ruby-full -y
+cd /home/ubuntu
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo systemctl status codedeploy-agent
+
+# Clean previous deployment
+rm -rf /var/www/html/*
