194679492a41a1db42a90fae479b328f58d6483f
Hayvid <davidhyppolitedh@gmail.com>
2024-12-27 13:28:26 -0500

    removed code deploy

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -12,13 +12,5 @@ sudo systemctl start nginx
 # Install Node.js from Amazon
 sudo apt-get install nodejs -y
 
-# Install CodeDeploy Agent
-sudo apt install ruby-full -y
-cd /home/ubuntu
-wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
-sudo systemctl status codedeploy-agent
-
 # Clean previous deployment
 sudo rm -rf /var/www/html/*
